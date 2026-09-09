322ddfa17c6757366087c81c2a96a9beab10f408
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 18:53:50 +0300

    Remove unused window import

--- mobile/src/main/java/com/skhealth/guardian/mobile/UiStyle.kt
+++ mobile/src/main/java/com/skhealth/guardian/mobile/UiStyle.kt
@@ -7,7 +7,6 @@ import android.graphics.drawable.GradientDrawable
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import android.widget.*
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
