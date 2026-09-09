9a0da25e6c8914817d8674dc1f9c5361b9171cf8
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 10:22:45 +0300

    fix: align shared JVM target to Java 17

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,11 +2,17 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.skhealth.guardian.shared"
     compileSdk = 36
     defaultConfig { minSdk = 26 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
+
 dependencies {
     testImplementation("junit:junit:4.13.2")
 }
