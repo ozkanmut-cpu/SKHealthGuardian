ccc9101f6b89a3ca26a22d25b0b4989cf73e2701
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 17:36:47 +0300

    Bump mobile version to 0.8.0

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.skhealth.guardian.mobile"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.7.0"
+        versionCode = 8
+        versionName = "0.8.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
